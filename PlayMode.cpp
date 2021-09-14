5985ceed16e79073a4ff8201f5783224e1ffcb63
gcwhitfield <gwhitfieldjr@gmail.com>
2021-09-14 03:17:46 -0400

    Revert to old playmode

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -1,5 +1,4 @@
 #include "PlayMode.hpp"
-#include "AssetImporter.hpp"
 
 //for the GL_ERRORS() macro:
 #include "gl_errors.hpp"
@@ -9,97 +8,8 @@
 
 #include <random>
 
-#define ERROR_FLOAT 0.000005f
-
 PlayMode::PlayMode() {
-
-	//Load in new files, and clean out the old script!
-	AssetImporter importer;
-
-	//Regenerate all of our files!
-	importer.WritePngsToFile();
-
-	//Create a new atlas, load all saved pngs into it.
-	importer.LoadTiles(atlas);
-	walls_at.push_back(glm::vec2(PPU466::ScreenWidth / 2, 1)); // test wall
-
-
-	// load from the PNG
-	std::array<TileRef, PPU466::BackgroundHeight *PPU466::BackgroundWidth> packedBackground = importer.GetPackedBackgroundFromPNG("Map.png");
-	for (int i = 0; i < 64; i++)
-	{
-		for (int j = 0; j < 60; j++)
-		{
-			unsigned index = 64 * i + j;
-			if (packedBackground[i].name == "wall")
-			{
-				ppu.background[index] = 0;
-			}
-		}
-	}
-
-	// step 1) read the tiles form the asset atlas
-	// importer.LoadBackground(atlas);
-	
-	//importer.LoadLevel(atlas, "levels/dummy_level/png");
-
-	// step 2) populate the ppu tiles, sprite locations, and background
-	for (int i = 0; i < atlas.tiles.size(); i++)
-	{
-		ppu.tile_table[i].bit0 = atlas.tiles[i].bit0;
-		ppu.tile_table[i].bit1 = atlas.tiles[i].bit1;
-	}
-
-	{
-		std::array<TileRef, PPU466::BackgroundHeight * PPU466::BackgroundWidth> level_tiles = importer.GetPackedBackgroundFromPNG("levels/dummy_level");
-				/*
-		for (int i = 0; i < 64; i ++)
-		{
-			for (int j = 0; j < 60; j++)
-			{
-				int index = 64 * i + j;
-				if (level_tiles[index].name == "player")
-				{
-					player_at.x = j;
-					player_at.y = i;
-				} else if (level_tiles[index].name == "light0")
-				{
-					lights_at.push_back(glm::vec2(j,i));
-				} else if (level_tiles[index].name == "light1")
-				{
-					lights_at.push_back(glm::vec2(j,i));
-				} else if (level_tiles[index].name == "light2")
-				{
-					lights_at.push_back(glm::vec2(j,i));
-
-				} else if (level_tiles[index].name == "wall")
-				{
-					walls_at.push_back(glm::vec2(j,i));
-				} else if (level_tiles[index].name == "goal")
-				{
-					goal_at.x = j;
-					goal_at.y = i;
-				} else if (level_tiles[index].name == "default")
-				{
-
-				}
-			}
-		}
-				*/
-	}
-
-	//LevelRetType level = atlas.getLevel("Default");
-
-	for (int i = 0; i < curr_level.size(); i++)
-	{
-		// TileRef curr_tile = curr_level[i];
-
-		// if the curr tile is a light, add to lights_at
-
-		// if the curr tile is a wall, add to walls_at
-	}
-	
-
+	//TODO:
 	// you *must* use an asset pipeline of some sort to generate tiles.
 	// don't hardcode them like this!
 	// or, at least, if you do hardcode them like this,
@@ -138,53 +48,6 @@ PlayMode::PlayMode() {
 		}
 	}
 
-	//wall sprite:
-	ppu.tile_table[default_wall_tile].bit0 = {
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-	};
-
-	// wall sprite:
-	ppu.tile_table[default_wall_tile].bit1 = {
-		0b00000000,
-		0b00000000,
-		0b00000000,
-		0b00000000,
-		0b00000000,
-		0b00000000,
-		0b00000000,
-		0b00000000,
-	};
-	//light sprite:
-	ppu.tile_table[default_wall_tile].bit0 = {
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-	};
-
-	// light sprite:
-	ppu.tile_table[default_wall_tile].bit1 = {
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-	};
-
 	//use sprite 32 as a "player":
 	ppu.tile_table[32].bit0 = {
 		0b01111110,
@@ -199,34 +62,12 @@ PlayMode::PlayMode() {
 	ppu.tile_table[32].bit1 = {
 		0b00000000,
 		0b00000000,
-		0b00100100,
 		0b00011000,
-		0b00000000,
-		0b01000010,
 		0b00100100,
-		0b01000010,
-	};
-
-	//use sprite 32 as a "player true":
-	ppu.tile_table[player_tile_index].bit0 = {
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-		0b11111111,
-	};
-	ppu.tile_table[player_tile_index].bit1 = {
 		0b00000000,
-		0b01111110,
-		0b10111101,
-		0b10111101,
-		0b11011011,
-		0b11011011,
-		0b11100111,
-		0b11100111,
+		0b00100100,
+		0b00000000,
+		0b00000000,
 	};
 
 	//makes the outside of tiles 0-16 solid:
@@ -245,33 +86,18 @@ PlayMode::PlayMode() {
 		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
 	};
 
-	//testing lighting
-	ppu.palette_table[2] = {
-		glm::u8vec4(0x00, 0x00, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0xff, 0xff), };
-
 	//used for the player:
 	ppu.palette_table[7] = {
 		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0x00, 0xff, 0xff),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-	};
-
-	//wall:
-	ppu.palette_table[default_wall_pallete] = {
-		glm::u8vec4(0xff, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0xff, 0x00, 0x00),
-		glm::u8vec4(0x00, 0x00, 0xff, 0x00),
-		glm::u8vec4(0xff, 0xff, 0x00, 0x00),
+		glm::u8vec4(0xff, 0xff, 0x00, 0xff),
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
+		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
 	};
 
 	//used for the misc other sprites:
 	ppu.palette_table[6] = {
 		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
-		glm::u8vec4(0xff, 0x00, 0x00, 0xff),
+		glm::u8vec4(0x88, 0x88, 0xff, 0xff),
 		glm::u8vec4(0x00, 0x00, 0x00, 0xff),
 		glm::u8vec4(0x00, 0x00, 0x00, 0x00),
 	};
@@ -279,7 +105,6 @@ PlayMode::PlayMode() {
 }
 
 PlayMode::~PlayMode() {
-	
 }
 
 bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size) {
@@ -302,11 +127,6 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 			down.pressed = true;
 			return true;
 		}
-		else if (evt.key.keysym.sym == SDLK_SPACE) {
-			space.downs += 1;
-			space.pressed = true;
-			return true;
-		}
 	} else if (evt.type == SDL_KEYUP) {
 		if (evt.key.keysym.sym == SDLK_LEFT) {
 			left.pressed = false;
@@ -317,14 +137,8 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 		} else if (evt.key.keysym.sym == SDLK_UP) {
 			up.pressed = false;
 			return true;
-		}
-		else if (evt.key.keysym.sym == SDLK_DOWN) {
+		} else if (evt.key.keysym.sym == SDLK_DOWN) {
 			down.pressed = false;
-			return true;
-		}
-		else if (evt.key.keysym.sym == SDLK_SPACE)
-		{
-			space.pressed = false;
 			return true;
 		}
 	}
@@ -339,304 +153,23 @@ void PlayMode::update(float elapsed) {
 	background_fade += elapsed / 10.0f;
 	background_fade -= std::floor(background_fade);
 
-	{ //handle player collisions with walls
-		for (auto w = walls_at.begin(); w < walls_at.end(); w++)
-		{
-			unsigned player_at_floor_x = (int)floor(player_at.x);
-			unsigned player_at_floor_y = (int)floor(player_at.y);
-			if (player_at_floor_x + 8 == (*w).x && (*w).y - 7 <= player_at_floor_y && player_at_floor_y <= (*w).y + 7)
-			{ //collides to the left of (*w)
-				player_at.x = (*w).x - 8;
-			} else if (player_at_floor_x - 8  + 1 == (*w).x && (*w).y - 7 <= player_at_floor_y && player_at_floor_y <= (*w).y + 7)
-			{ //collides to the right
-				player_at.x = (*w).x + 8;
-			} else if (player_at_floor_y - 8 + 1 == (*w).y && (*w).x - 7 <= player_at_floor_x && player_at_floor_x <= (*w).x + 7)
-			{ //collides above
-				grounded = true;
-				player_at.y = (*w).y + 8;
-				player_velocity.y = 0;
-			} else if (player_at_floor_y + 8 == (*w).y && (*w).x - 7 <= player_at_floor_x && player_at_floor_x <= (*w).x + 7)
-			{ //collides below
-				player_at.y = (*w).y - 8;
-			}
-		}
-	}
-
 	constexpr float PlayerSpeed = 30.0f;
-	player_velocity.x = 0;
-	if (left.pressed) player_velocity.x -= PlayerSpeed;
-	if (right.pressed) player_velocity.x += PlayerSpeed;
+	if (left.pressed) player_at.x -= PlayerSpeed * elapsed;
+	if (right.pressed) player_at.x += PlayerSpeed * elapsed;
+	if (down.pressed) player_at.y -= PlayerSpeed * elapsed;
+	if (up.pressed) player_at.y += PlayerSpeed * elapsed;
 
-	if (space.pressed && space.held < max_jump_time && grounded)
-	{
-		jumping = true;
-		space.held += elapsed;
-		if (space.held < max_jump_time)
-		{
-			player_velocity.y = jump_speed;
-		}
-	}
-	else
-	{
-		grounded = false;
-		if (space.held > 0.0f && jumping)
-		{
-			player_velocity.y = 0.0f;
-			jumping = false;
-		}
-		if (!space.pressed)
-		{
-			space.held = 0.0f;
-		}
-	}
-
-	player_velocity.y += gravity * elapsed;
-
-	//if (down.pressed) player_at.y -= PlayerSpeed * elapsed;
-	//if (up.pressed) player_at.y += PlayerSpeed * elapsed;
-
-	player_at += player_velocity * elapsed;
-
-	//Bounds check!
-	if (player_at.y <= 0)
-	{
-		player_at.y = 0;
-		player_velocity.y = 0;
-		grounded = true;
-	}
-	
 	//reset button press counters:
 	left.downs = 0;
 	right.downs = 0;
 	up.downs = 0;
 	down.downs = 0;
-	space.downs = 0;
-
-	//if player collides with goal, win the game
-	if (false) // TODO: replace with goal collision
-	{
-		level_complete();
-		next_state = WIN;
-	}
-	
-	//TODO: if player collides with a light, subtract health
-	//TODO: if the player is in shadow, add health
 }
 
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
-	
-	switch(curr_state)
-	{
-		case GAMEPLAY:
-			draw_gameplay();
-			break;
-		case WIN:
-			draw_win();
-			break;
-		case DEAD:
-			draw_dead();
-			break;
-	}
-	//--- actually draw ---
-	ppu.draw(drawable_size);
-}
-
-//Lighting
-
-//End goal is to use the following but may not have time to test. For now will use more basic but still viable method (testing center only)
-
-//Checks if object is with in outer or inner range of spotlight, and gives lighting value accordingly
-/*uint8_t PlayMode::whichLight(glm::vec2 lightPos, glm::vec2 objPos, float innerTheta, float outerTheta) {
-	if (objPos.y + 4.f > lightPos.y) return 0;
-	assert(innerTheta != 0.0f && innerTheta != 90.0f && outerTheta != 0.0f && outerTheta != 90.0f);
-
-	//Derived from https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-box-intersection
-
-	auto getVal = [this](glm::vec2 lightPos, glm::vec2 objPos, float theta){
-		
-		float minX = objPos.x + 4.f;
-		float maxX = objPos.x - 4.f;
-		if (abs(lightPos.x - (objPos.x - 4.f)) < abs(lightPos.x - (objPos.x + 4.f))) {
-			minX = objPos.x - 4.f; maxX = objPos.x + 4.f;
-		}
-		glm::vec2 b0 = glm::vec2(minX, objPos.y + 4.f); //close
-		glm::vec2 b1 = glm::vec2(maxX, objPos.y - 4.f); //far	
-
-
-		bool doesLeft = true;
-		bool doesRight = true;
-		float tx = (b0.x - lightPos.x) / -cos(theta);//? is this dX?
-		float ty = (b0.y - lightPos.y) / -sin(theta);
-		float tMinInLeft = (tx > ty) ? tx : ty;
-		float t1x = (b1.x - lightPos.x) / -cos(theta);//? is this dX?
-		float t1y = (b1.y - lightPos.y) / -sin(theta);
-		float tMaxInLeft = (tx < ty) ? tx : ty;
-		//intersection check
-		if (tx > t1y || ty > t1x) doesLeft = false;
-
-		tx = (b0.x - lightPos.x) / cos(theta);//? is this dX?
-		ty = (b0.y - lightPos.y) / -sin(theta);
-		float tMinInRight = (tx > ty) ? tx : ty;
-		t1x = (b1.x - lightPos.x) / cos(theta);//? is this dX?
-		t1y = (b1.y - lightPos.y) / -sin(theta);
-		float tMaxInRight = (tx < ty) ? tx : ty;
-		//intersection check
-		if (tx > t1y || ty > t1x) doesRight = false;
-		//Inner val
-		float retVal = 0.0f;
-		if (doesLeft && doesRight) { //If both left and right intersect box
-			glm::vec2 minIntLeft = glm::vec2(-tMinInLeft * cos(innerTheta) + lightPos.x,
-			-tMinInLeft * sin(innerTheta) + lightPos.y);
-			glm::vec2 maxIntLeft = glm::vec2(-tMaxInLeft * cos(innerTheta) + lightPos.x,
-			-tMaxInLeft * sin(innerTheta) + lightPos.y);
-			glm::vec2 minIntRight = glm::vec2(tMinInRight * cos(innerTheta) + lightPos.x,
-			-tMinInRight * sin(innerTheta) + lightPos.y);
-			glm::vec2 maxIntRight = glm::vec2(tMaxInRight * cos(innerTheta) + lightPos.x,
-			-tMaxInRight * sin(innerTheta) + lightPos.y);
-
-			//Due to location of light,we know min hits are on top, and max are on sides
-			retVal = 64.f;
-			retVal -= (minIntLeft.x - objPos.x + 4.f) * (maxIntLeft.y - objPos.y + 4.f) / 2;
-			retVal -= (objPos.x + 4.f - minIntRight.x) * (objPos.y + 4.f - maxIntRight.y) / 2;
-			retVal /= 64.f;
-
-		}
-		else if (doesLeft) { //If only left does
-			if(minIntLeft.y >= objPos.x + 4.f - ERROR_FLOAT){
-				retVal = 64.f;
-				retVal -= (minIntLeft.x - objPos.x + 4.f) * (maxIntLeft.y - objPos.y + 4.f) / 2;
-				retVal /= 64.f;
-			}
-			else{
-				retVal = 64.f;
-				retVal -= (manIntLeft.x - objPos.x + 4.f) * (minIntLeft.y - objPos.y + 4.f) / 2;
-				retVal/= 64.fl
-			}
-		}
-		else if (doesRight) { //If only right does, consider left the left side of box
-			if(minIntRight.y >= objPos.x + 4.f - ERROR_FLOAT){
-				retVal = 64.f;
-				retVal -= (objPos.x + 4.f - minIntRight.x) * (objPos.y + 4.f - maxIntRight.y) / 2;
-				retVal /= 64.f;
-			}
-			else{
-				retVal = 64.f;
-				retVal -= (objPos.x + 4.f - maxIntRight.x) * (objPos.y + 4.f - minIntRight.y) / 2;
-				retVal/= 64.f;
-			}
-		}
-		return retVal;
-	};
-
-	float innerVal = 2.0f*getVal(lightPos, objPos, innerTheta);
-	float outerVal = getVal(lightPos, objPos, outerTheta) - innerVal/2;
-	return (uint8_t) roundf(innerVal + outerVal);
-}*/
-/*
-//More basic version of above for testing/if cant test ^^^ in time
-// uint8_t PlayMode::whichLight(glm::vec2 lightPos, glm::vec2 objPos, float innerTheta, float outerTheta) {
-	//If above, don't render as lit
-	if (objPos.y >= lightPos.y) return 0;
-	//First check inner occlusion
-	float xRange = (lightPos.y - objPos.y) / tan(innerTheta);
-	if (fabs(lightPos.x - objPos.x) <= xRange) return 2;
-	//Then check outer occlusion
-	xRange = (lightPos.y - objPos.y) / tan(outerTheta);
-	if (fabs(lightPos.x - objPos.x) <= xRange) return 1;
-	return 0;
-}
-
-void PlayMode::updatePallet() {
-	for (size_t x = 0; x < PPU466::BackgroundWidth; x++) {
-		for (size_t y = 0; y < PPU466::BackgroundHeight; y++) {
-			glm::vec2 objPos = glm::vec2((float)(x * 8 + 4), (float)(y * 8 + 4));
-			uint8_t lightVal = 0;
-			for (size_t lightInd = 0; lightInd < lights_at.size(); lightInd++) {
-				uint8_t tempVal = whichLight(lights_at[lightInd], objPos, (LightTypeInd.getObj(lights_type[lightInd])).inner, 
-					(LightTypeInd.getObj(lights_type[lightInd])).outer);
-				if (tempVal > lightVal) lightVal = tempVal;
-			}
-			size_t ind = y * PPU466::BackgroundWidth + x;
-			curr_bg_palletes[ind] = backgroundColors[lightVal][ind]; 
-		}
-	}
-}
-*/
-//x,y, in [256,240]
-//uint8_t PlayMode::updateLightLevel(size_t objInd) {
-//	uint8_t lightVal = 0;
-//	for (size_t lightInd = 0; lightInd < lights_at.size(); lightInd++) {
-//		uint8_t tempVal = whichLight(glm::vec2((float)((lights_at[lightInd]).x + 4.f), (float)((lights_at[lightInd]).y + 4.f)),
-//			glm::vec2((float)((walls_at[objInd]).x + 4.f),(float)((walls_at[objInd]).y + 4.f)),
-//			(LightTypeInd.getObj( /*lights_type[lightInd]*/"light1")).inner, (LightTypeInd.getObj(/*lights_type[whichLight]*/"light1")).outer);
-//		if (tempVal > lightVal) lightVal = tempVal;
-//	}
-//	std::array<uint8_t, 3> wallPalletes;
-//	wallPalletes[0] = 0;//dark
-//	wallPalletes[1] = 1;//partially lit
-//	wallPalletes[2] = 2;//lit
-//	return wallPalletes[lightVal];
-//}
-
-
-void PlayMode::level_complete()
-{
-	std::cout << "The level has been completed!" << std::endl;
-}
-
-void PlayMode::player_died()
-{
-	std::cout << "The player has died! :(" << std::endl;
-}
-
-void PlayMode::draw_gameplay()
-{
-	//--- set ppu state based on game state --
-	for (int i = 0; i < 64; i++)
-	{
-		for (int j = 0; j < 60; j++)
-		{
-			unsigned index = 64 * i + j;
-			//curr_bg[index].bit0 = atlas.getBG("Default").tiles[index].bit0;
-			//curr_bg[index].bit1 = atlas.getBG("Default").tiles[index].bit1;
-
-			ppu.background[index] = 0;
-			// set the background
-			/*
-			ppu.background[index] = (bg_pallete_index << 8) + index;
-			unsigned long bg_pallete_index = 3;
-
-			if (level_tiles[index].name == "player")
-			{
-				player_at.x = j;
-				player_at.y = i;
-			} else if (level_tiles[index].name == "light0")
-			{
-				lights_at.push_back(glm::vec2(j,i));
-			} else if (level_tiles[index].name == "light1")
-			{
-				lights_at.push_back(glm::vec2(j,i));
-			} else if (level_tiles[index].name == "light2")
-			{
-				lights_at.push_back(glm::vec2(j,i));
-
-			} else if (level_tiles[index].name == "wall")
-			{
-				walls_at.push_back(glm::vec2(j,i));
-			} else if (level_tiles[index].name == "goal")
-			{
-				goal_at.x = j;
-				goal_at.y = i;
-			} else if (level_tiles[index].name == "default")
-			{
-
-			}
-			*/
-		}
-	}
+	//--- set ppu state based on game state ---
 
 	//background color will be some hsv-like fade:
-	//To be replaced with current background
 	ppu.background_color = glm::u8vec4(
 		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 0.0f / 3.0f) ) ) ))),
 		std::min(255,std::max(0,int32_t(255 * 0.5f * (0.5f + std::sin( 2.0f * M_PI * (background_fade + 1.0f / 3.0f) ) ) ))),
@@ -658,38 +191,13 @@ void PlayMode::draw_gameplay()
 	ppu.background_position.y = int32_t(-0.5f * player_at.y);
 
 	//player sprite:
-	ppu.sprites[player_sprite_index].x = int32_t(player_at.x);
-	ppu.sprites[player_sprite_index].y = int32_t(player_at.y);
-	ppu.sprites[player_sprite_index].index = player_tile_index;
-	ppu.sprites[player_sprite_index].attributes = 7;
+	ppu.sprites[0].x = int32_t(player_at.x);
+	ppu.sprites[0].y = int32_t(player_at.y);
+	ppu.sprites[0].index = 32;
+	ppu.sprites[0].attributes = 7;
 
-	//goal sprite
-	ppu.sprites[1].x = int32_t(goal_at.x);
-	ppu.sprites[1].y = int32_t(goal_at.y);
-	ppu.sprites[1].index = goal_sprite_index;
-	ppu.sprites[1].attributes = 7; 
-
-	//platforms and walls
-	int i;
-	for (i = 2; i < walls_at.size() + 2; i++)
-	{
-		(ppu.sprites[i]).x = (uint8_t)(walls_at[i - 2]).x;
-		(ppu.sprites[i]).y = (uint8_t)(walls_at[i - 2]).y;
-		ppu.sprites[i].index = default_wall_tile;
-		ppu.sprites[i].attributes = 2; //updateLightLevel(i-2);
-	}
-
-	//lights
-	for (int j = i; j < i + lights_at.size(); j++)
-	{
-		ppu.sprites[j].x = (uint8_t)lights_at[j - i].x;
-		ppu.sprites[j].y = (uint8_t)walls_at[j - i].y;
-		ppu.sprites[j].index = default_light_tile;
-		ppu.sprites[j].attributes = default_wall_pallete; 
-	}
-	
 	//some other misc sprites:
-	for (uint32_t i = 3; i < 63; ++i) {
+	for (uint32_t i = 1; i < 63; ++i) {
 		float amt = (i + 2.0f * background_fade) / 62.0f;
 		ppu.sprites[i].x = int32_t(0.5f * PPU466::ScreenWidth + std::cos( 2.0f * M_PI * amt * 5.0f + 0.01f * player_at.x) * 0.4f * PPU466::ScreenWidth);
 		ppu.sprites[i].y = int32_t(0.5f * PPU466::ScreenHeight + std::sin( 2.0f * M_PI * amt * 3.0f + 0.01f * player_at.y) * 0.4f * PPU466::ScreenWidth);
@@ -698,14 +206,6 @@ void PlayMode::draw_gameplay()
 		if (i % 2) ppu.sprites[i].attributes |= 0x80; //'behind' bit
 	}
 
-}
-
-void PlayMode::draw_win()
-{
-
-}
-
-void PlayMode::draw_dead()
-{
-
+	//--- actually draw ---
+	ppu.draw(drawable_size);
 }
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -1,38 +1,14 @@
 #include "PPU466.hpp"
 #include "Mode.hpp"
-#include "AssetLoad.h"
-#include "AssetImporter.hpp"
 
 #include <glm/glm.hpp>
 
 #include <vector>
 #include <deque>
-#include "ObjectTypes.hpp"
 
-/*
- *
- * PlayMode needs to know several things about the game state:
- * 1) the light level of each sprite. 0 = no light, 7 = max light
- * 2) collisions of each sprite. Can the player collide with the sprite? Y or N?
- * 
- */
 struct PlayMode : Mode {
 	PlayMode();
 	virtual ~PlayMode();
-
-	enum GameState
-	{
-		GAMEPLAY,
-		WIN,
-		DEAD
-	};
-
-	GameState curr_state;
-	GameState next_state;
-
-	// ----- asset importing/loading -----
-	AssetAtlas atlas;
-	AssetImporter importer;
 
 	//functions called by main loop:
 	virtual bool handle_event(SDL_Event const &, glm::uvec2 const &window_size) override;
@@ -40,67 +16,20 @@ struct PlayMode : Mode {
 	virtual void draw(glm::uvec2 const &drawable_size) override;
 
 	//----- game state -----
-	//player position:
-	int SPRITES_MAX = 64;
-	glm::vec2 player_at = glm::vec2(0, 232);
-	std::vector<glm::vec2> walls_at = std::vector<glm::vec2>(SPRITES_MAX);
-	std::vector<glm::vec2> lights_at = std::vector<glm::vec2>(SPRITES_MAX);
-	std::vector<std::string> lights_type = std::vector<std::string>(SPRITES_MAX); //Fill in with light type strings we want to use
-	
-	//Can we also have a vector the same size as lights_at which indicates the light type? Could be a Light object reference
-
-	//player velocity
-	glm::vec2 player_velocity = glm::vec2(0.0f);
-	unsigned player_tile_index = 33;
-	unsigned player_sprite_index = 0;
-	glm::vec2 goal_at = glm::vec2(248, 0);
-	unsigned goal_sprite_index = 34;
-
-	int health; //the player dies when health is less than zero
-	
-	// the index into the light_levels array must be the same index into the ppu.sprites array
-	std::array<unsigned short, 64> light_levels; // the light levels of each block
-
-	void level_complete(); // TODO
-	void subtract_health(int amt) {health -= amt; if (health < 0) player_died(); }
-	void player_died(); // TODO
-
-	float gravity = -120.0f;
-	float max_jump_time = 0.4f;
-	float jump_speed = 60.0f;
-	bool jumping = false;
-	bool grounded = false;
 
 	//input tracking:
 	struct Button {
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-		float held = 0.0f; //Only used for spacebar!
-	} left, right, down, up, space;
+	} left, right, down, up;
 
 	//some weird background animation:
 	float background_fade = 0.0f;
 
-	//Lighting
-	ObjectTypes LightTypeInd = ObjectTypes();
-	std::array<std::array< uint8_t, PPU466::BackgroundWidth* PPU466::BackgroundHeight>
-		, 3> backgroundColors; //0 = dark, 1 = partially lit, 2 = lit, each an array of pallets
-	//Each object should have 3 pallets stored with it, which are swapped out depending on its lighting value.
-	// uint8_t whichLight(glm::vec2 lightPos, glm::vec2 objPos, float innerTheta, float outerTheta);
-	// void updatePallet();
-	// uint8_t updateLightLevel(size_t objInd);
-
+	//player position:
+	glm::vec2 player_at = glm::vec2(0.0f);
 
 	//----- drawing handled by PPU466 -----
+
 	PPU466 ppu;
-	//current background
-	std::array< TileAssetData, PPU466::BackgroundWidth* PPU466::BackgroundHeight > curr_bg;
-	std::array< uint8_t, PPU466::BackgroundWidth* PPU466::BackgroundHeight > curr_bg_palletes;
-	std::array< TileRef, PPU466::BackgroundWidth* PPU466::BackgroundHeight > curr_level;
-	unsigned default_wall_tile = 35;
-	unsigned default_light_tile = 36;
-	unsigned default_wall_pallete = 8;
-	void draw_gameplay();
-	void draw_win();
-	void draw_dead();
 };
