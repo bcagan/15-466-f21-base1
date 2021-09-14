6b5ac1d1c780a036aeff2306c2c575f54bdf7f7a
bcagan <30479930+bcagan@users.noreply.github.com>
2021-09-13 21:16:36 -0400

    Object Search added
    ^^^

---
+++ ObjectTypes.cpp
@@ -0,0 +1,11 @@
+#include"ObjectTypes.hpp"
+#include <vector>
+#include <string>
+
+//Searches objlist for requested object info, if none exists, returns default
+Object ObjectTypes::getObj(std::string objType) {
+	for (size_t curObj = 0; curObj < objList.size(); curObj++) {
+		if (objType.compare((objList[curObj]).name) == 0) return objList[curObj];
+	}
+	return defaultObj;
+}
--- ObjectTypes.hpp
+++ ObjectTypes.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <glm/gtc/type_ptr.hpp>
 #include "AssetLoad.h"
 class Object {
@@ -12,18 +13,15 @@ public:
 	float outer;
 };
 
-//Light, wall, goal
-
-
 //TO DO
-//Set up pallets
 //Set up function that references object type and gets updated pallet from that (not lights)
+//Set up in game actual object "list"/renderer
 //Test test test!
-//Seatch (get object)
 
 class ObjectTypes {
+public:
 	ObjectTypes() {
-		objList.resize(5);
+		objList.resize(6);
 
 		Object wall;
 		Object goal;
@@ -31,59 +29,75 @@ class ObjectTypes {
 		LightObj light1;
 		LightObj light2;
 
-		wall.name = "wall";
 		TileRef pallet0;
 		TileRef pallet1;
 		TileRef pallet2;
+		defaultObj.name = "default";
+		pallet0.name = "default";
+		pallet1.name = "default";
+		pallet2.name = "default";
+		defaultObj.pallets[0] = pallet0;
+		defaultObj.pallets[1] = pallet1;
+		defaultObj.pallets[2] = pallet2;
+		wall.name = "wall";
 		pallet0.name = "wall";
 		pallet1.name = "wall";
 		pallet2.name = "wall";
-		wall.pallets[0] pallet0;
-		wall.pallets[1] pallet1;
-		wall.pallets[2] pallet2;
+		wall.pallets[0] = pallet0;
+		wall.pallets[1] = pallet1;
+		wall.pallets[2] = pallet2;
 		goal.name = "goal";
 		pallet0.name = "goal";
 		pallet1.name = "goal";
 		pallet2.name = "goal";
-		goal.pallets[0] pallet0;
-		goal.pallets[1] pallet1;
-		goal.pallets[2] pallet2;
+		goal.pallets[0] = pallet0;
+		goal.pallets[1] = pallet1;
+		goal.pallets[2] = pallet2;
 		light0.name = "light0";
 		pallet0.name = "light";
 		pallet1.name = "light";
 		pallet2.name = "light";
-		light0.pallets[0] pallet0;
-		light0.pallets[1] pallet1;
-		light0.pallets[2] pallet2;
+		light0.pallets[0] = pallet0;
+		light0.pallets[1] = pallet1;
+		light0.pallets[2] = pallet2;
 		light0.inner = 45.f / 2.f;
 		light0.outer = 45.5;
 		light1.name = "light1";
 		pallet0.name = "light";
 		pallet1.name = "light";
 		pallet2.name = "light";
-		light1.pallets[0] pallet0;
-		light1.pallets[1] pallet1;
-		light1.pallets[2] pallet2;
+		light1.pallets[0] = pallet0;
+		light1.pallets[1] = pallet1;
+		light1.pallets[2] = pallet2;
 		light1.inner = 45.f / 2.f;
 		light1.outer = 45.5;
 		light2.name = "light2";
 		pallet0.name = "light";
 		pallet1.name = "light";
 		pallet2.name = "light";
-		light2.pallets[0] pallet0;
-		light2.pallets[1] pallet1;
-		light2.pallets[2] pallet2;
+		light2.pallets[0] = pallet0;
+		light2.pallets[1] = pallet1;
+		light2.pallets[2] = pallet2;
 		light2.inner = 45.f / 2.f;
 		light2.outer = 45.5;
+		objList[0] = defaultObj;
+		objList[1] = wall;
+		objList[2] = goal;
+		objList[3] = light0;
+		objList[4] = light1;
+		objList[5] = light2;
 	}
 	~ObjectTypes();
 	Object getObj(std::string objType);
+private:
+	Object defaultObj;
 	std::vector<Object> objList;
 };
 
-//Objects referenced by size_t
+//Objects referenced by string
 //"light0"
 //"light1"
 //"light2"
 //"wall"
 //"goal"
+//default
